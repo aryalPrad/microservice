3d6a8d2dc4d009855afbf3cb4bb5a108346fb115
Pradhumna <pradhumna.aryal07@gmail.com>
2023-12-03 21:53:43 -0600

    spring boot authentication added

---
+++ doctor-service/src/main/java/com/vastika/doctorservice/configuration/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.vastika.doctorservice.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final String[] PERMIT_URL = {
+            "/api/v1/ping"
+    };
+
+    @Bean
+    public SecurityFilterChain applicationSecurity(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .cors().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .formLogin().disable()
+                .authorizeHttpRequests(registry ->
+                        registry
+                                .regexMatchers(PERMIT_URL).permitAll()
+                                .anyRequest().authenticated()
+                );
+
+        return httpSecurity.build();
+    }
+
+}
